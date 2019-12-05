3c5744eeaaf4fcbb907fe48bb4b8e0681c33e2cf
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-05 20:08:18 +0100

    Write analyze

---
+++ functions/analyse.js
@@ -0,0 +1,15 @@
+const analyse = (array) => {
+  let min = array[0];
+  let max = array[0];
+  let average = 0;
+  const length = array.length
+  for (let i = 0; i < array.length; i++){
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+    average += array[i];
+  }
+  average = average/length
+  return {
+    min, max, average, length
+  }
+}
